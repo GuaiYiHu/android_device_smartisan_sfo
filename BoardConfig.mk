94d21d45acb2a40e17e84371e15cc3193ce520e6
GuaiYiHu <guaiyihu@foxmail.com>
2019-06-18 13:43:20 +0800

    cancro: Kill cmhw
    Change-Id: I770bbaae0c8b847672c9359f48468e53e8549ee6

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -146,9 +146,6 @@ BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_23x41.h\"
 TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
 TARGET_RECOVERY_LCD_BACKLIGHT_PATH := \"/sys/class/leds/lcd-backlight/brightness\"
 
-# CM Hardware
-BOARD_HARDWARE_CLASS += $(CANCRO_PATH)/cmhw
-
 # No old RPC for prop
 TARGET_NO_RPC := true
 

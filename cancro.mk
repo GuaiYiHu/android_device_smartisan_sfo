3d4161395212bfb2f0792db3c58768c9e301d665
GuaiYiHu <guaiyihu@foxmail.com>
2019-06-21 01:58:49 +0800

    Revert "cancro: Switch to Mokee's custom vibrator HAL"
    This reverts commit eba3c06f6ed509a783a9efc849c316599168bda9.
    
    Change-Id: I6d96fe038ec916a98041b3d9fa6bd1039529b656

--- cancro.mk
+++ cancro.mk
@@ -70,7 +70,7 @@ PRODUCT_PACKAGES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    android.hardware.vibrator@1.0-service.mokee
+    android.hardware.vibrator@1.0-impl
 
 #USB HAL
 PRODUCT_PACKAGES += \

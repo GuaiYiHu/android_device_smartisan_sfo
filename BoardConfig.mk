4ea024d7ac1a6597f2b95ef9140b3a6016af2f97
Arian <arian.kulmer@web.de>
2020-09-12 12:33:48 +0200

    sfo: Allow building with copy headers for now
    Change-Id: Ic2ac8483e723a5180d4a34c9b8e85f72654f8875
    Signed-off-by: Mohammad Hasan Keramat J <ikeramat80@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -14,6 +14,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+BUILD_BROKEN_USES_BUILD_COPY_HEADERS := true
+
 BOARD_VENDOR := smartisan
 
 DEVICE_PATH := device/smartisan/sfo

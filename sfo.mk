1da5d3abb915767dbef7cfc65a179dca5a0487e0
Arian <arian.kulmer@web.de>
2020-09-22 20:06:14 +0200

    sfo: Build new aidl qti power HAL
    Change-Id: I4bdea354e54d34d0dfe9b811e228fc6f8762d2ed

--- sfo.mk
+++ sfo.mk
@@ -61,7 +61,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.2-service-qti
+    android.hardware.power-service-qti
 
 # Privapp Whitelist
 PRODUCT_COPY_FILES += \
